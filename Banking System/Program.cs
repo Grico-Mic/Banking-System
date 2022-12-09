23972a093e8e44be74cd8b206baeae21cf5a56dc
Grico-Mic <107273766+Grico-Mic@users.noreply.github.com>
2022-12-09 20:59:58 +0100

    Create Menu

--- Banking System/Program.cs
+++ Banking System/Program.cs
@@ -6,7 +6,63 @@ namespace Banking_System
     {
         static void Main(string[] args)
         {
+            var ShouldContinue = "";
+            do
+            {
+                var userInput = ShowMenu();
+
+                switch (userInput)
+                {
+                    case "1":
+                        Console.WriteLine("Create new account is in proces");
+                        break;
+                    case "2":
+                        Console.WriteLine("View all accounts is in proces");
+                        break;
+                    case "3":
+                        Console.WriteLine("View account is in proces");
+                        break;
+                    case "4":
+                        Console.WriteLine("Create transaction is in proces");
+                        break;
+                    case "5":
+                        Console.WriteLine("View all transaction is in proces");
+                        break;
+                    case "6":
+                        Console.WriteLine("Get provision revenue is in proces");
+                        break;
+                    case "7":
+                        Console.WriteLine("Get all transaction amount is in proces");
+                        break;
+
+                    default:
+                        Console.WriteLine("Wrong Input");
+                        break;
+                }
+                Console.WriteLine("Would you like to continue?Enter NO for exit");
+
+                ShouldContinue = Console.ReadLine();
+
+            } while (ShouldContinue != "no");
+
+           
+
             
+        }
+
+        static string ShowMenu()
+        {
+            Console.WriteLine("Please choose one of the folowing options:");
+
+            Console.WriteLine("1.Create new account");
+            Console.WriteLine("2.View all accounts");
+            Console.WriteLine("3.View account");
+            Console.WriteLine("4.Create transaction");
+            Console.WriteLine("5.View all transaction");
+            Console.WriteLine("6.Get provision revenue");
+            Console.WriteLine("7.Get all transaction amount");
+
+            return  Console.ReadLine().Trim();
         }
     }
 }
