88142bef011510cd7b2667408f3440420b9a5c19
Grico-Mic <107273766+Grico-Mic@users.noreply.github.com>
2023-01-17 17:15:27 +0100

    Add class BankingSystemExceptions

---
+++ Banking System/Exceptions/BankingSystemExceptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace Banking_System.Exceptions
+{
+    public class BankingSystemExceptions : Exception
+    {
+        public BankingSystemExceptions (string message):base(message)
+        {
+
+        }
+    }
+}
